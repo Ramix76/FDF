f7f989c7b9c681f7684565e32a7ac9e7c815ad4e
Ramix76 <framos-p@student.42barcelona.com>
2023-01-09 15:14:36 +0100

    again

--- obj/draw_pixels.d
+++
@@ -1,3 +0,0 @@
-obj/draw_pixels.o: srcs/draw_pixels.c \
-  srcs/../lib/minilibx/minilibx_macos/mlx.h srcs/../inc/define.h \
-  srcs/../inc/utils.h srcs/../inc/../inc/define.h
--- obj/keys.d
+++
@@ -1,3 +0,0 @@
-obj/keys.o: srcs/keys.c srcs/../lib/minilibx/minilibx_macos/mlx.h \
-  srcs/../inc/define.h srcs/../inc/keycodes.h srcs/../inc/utils.h \
-  srcs/../inc/../inc/define.h
