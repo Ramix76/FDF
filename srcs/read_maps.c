bc56cdfd5414e7b23d67ca462831120bc5f3e72f
Ramix76 <framos-p@student.42barcelona.com>
2023-01-09 15:30:55 +0100

    loading

--- srcs/read_maps.c
+++ srcs/read_maps.c
@@ -6,7 +6,7 @@
 /*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 11:36:10 by framos-p          #+#    #+#             */
-/*   Updated: 2023/01/09 12:01:16 by framos-p         ###   ########.fr       */
+/*   Updated: 2023/01/09 15:30:27 by framos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,29 @@
 #include "../inc/utils.h"
 #include "../inc/check_errors.h"
 
+int	load_map(char *file_name, t_map *map)
+{
+	int		nline;
+	int		fd;
+	char	*line;
 
-
-
+	nline = 0;
+	map_init(map);
+	valid_map(file_name, map);
+	map -> points = ft_calloc(map -> total, sizeof(t_point));
+	if (map -> limits.axes[X] == 0)
+		terminate_map(ERR_EMPTY);
+	fd = open(file_name, O_RDONLY);
+	if (fd < 3 || map -> points == NULL)
+		return (0);
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		save_map_points(map, nline, line);
+		nline++;
+		free(line);
+		line = get_next_line(fd);
+		write(1, '.', 1);
+	}
+	return (1);
+}
